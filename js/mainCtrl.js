431cb453e4ee6aea51ab253f920a8355a392f346
Jared M. Peterson <jared@peteyinc.com>
2016-05-24 15:03:04 -0600

    finished dataService

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,3 +1,9 @@
 angular.module("quoteBook").controller("mainCtrl", function($scope, dataService) {
 
+  $scope.getQuotes = function() {
+    $scope.quotes = dataService.getQuotes();
+  }
+
+  $scope.getQuotes();
+
 });
